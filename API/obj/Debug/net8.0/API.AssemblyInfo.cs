dfc7dbad526dc292c661f57b697dab35a0f83f9a
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-09-25 19:55:09 -0400

    Packages installed
    - EntityFrameworkCore.Design
    - EntityFrameworkCore.Sqlite

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5763a7e2432e320992083c2e8dc30d07643af7b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+766fc06cfcc049b953f852add3f3f137972532c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

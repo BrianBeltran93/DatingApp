6cfb3cf8f09673332f4ed5f61df67c0380ecde4a
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-09-30 19:35:23 -0400

    First controller created
    UsersController created

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cc3627e88edaccf81eac15b3dd7ed06bee4eb96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c97ba1f3cfcf599cbed00ef6fd8100e1e04002bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

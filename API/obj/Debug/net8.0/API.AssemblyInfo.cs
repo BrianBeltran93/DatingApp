2d5c01c54de7ec0ecb27dfc56834a7699b01e8a7
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-09-30 21:16:02 -0400

    Added CORS support

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc6988ce0954d2089e3a20ba9e8841c4ad870d9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4432484b207bdc1309a447b48077edee09853400")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

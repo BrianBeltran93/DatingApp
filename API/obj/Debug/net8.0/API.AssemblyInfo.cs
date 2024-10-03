4f02e53625521b08c98a4776f044e489f827a6b7
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-10-02 20:48:02 -0400

    Added authentication middleware

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd1bbaf207b78e6244ee29f9fcf7632916bd878b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c3979cb1348b97ffc9c0a9680fcb35cabb535d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

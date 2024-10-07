9d47e370a51ac4bb3dd0db048797720cf3157ad5
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-10-06 20:01:35 -0400

    Navbar created

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f02e53625521b08c98a4776f044e489f827a6b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf3703cdd43042a04bc30d8cf57c13ea7d476cf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

e89908593cb752917a38bc28a44ff52bb3477b9a
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-10-06 21:13:00 -0400

    Account service injected into nav component

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d3e67b10a25f5acfbfaf656b77c3689d109a696")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4958130991cd7a700ae4f44eb5e84b48882c5aaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

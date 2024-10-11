9b131cce8e7057c6853c4ff9fb83e87fb11cedb5
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-10-10 21:08:06 -0400

    Angular route guard added

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ce258f044140225a202458c8722e4a77dab3e7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b24fe4c5719ded67d4b3d2134455c4cb89fbb075")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

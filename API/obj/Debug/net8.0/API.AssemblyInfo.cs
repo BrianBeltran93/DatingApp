107162b8e8606586e3f6e38c056790bdd7e21b15
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-10-10 20:34:54 -0400

    More components created
    - member-detail
    - member-list
    - messages
    - lists

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+843c2a6e32c691c0f9b7dc2b9f5a7701238914b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ce258f044140225a202458c8722e4a77dab3e7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

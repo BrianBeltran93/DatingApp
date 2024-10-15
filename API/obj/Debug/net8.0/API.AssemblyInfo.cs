1d41e7ea92800546a89029a847e98d25108a011c
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-10-14 20:31:02 -0400

    Error controller created
    - for testing errors

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b24fe4c5719ded67d4b3d2134455c4cb89fbb075")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee526df69780879fb9b0eed365369b0dfa7e7a51")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

502ae12c4eb0ee5c50a50f03d76ea869fabd0289
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-10-14 21:50:12 -0400

    Created Error interceptor

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+830c5334c8960f24f94015538680052a6842e956")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91d3ae1107ec94fb202dc9e6e821ece936258076")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

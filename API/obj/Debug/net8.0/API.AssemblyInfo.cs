c2ce57b5117daff7c832f076431272c3907dd9d9
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-10-15 22:05:01 -0400

    DateTime extension added to calculate age

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e25d5ff8469f3ac82947852b49006cf2db70814")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d09574278b8ebc8383292d56c42f652bdb0866c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

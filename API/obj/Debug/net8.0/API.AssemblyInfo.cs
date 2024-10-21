2a8d038105bb5131df0e129ab5a2e4c2fbe5c2ac
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-10-20 20:42:00 -0400

    Repopulated database with users

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d09574278b8ebc8383292d56c42f652bdb0866c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2ce57b5117daff7c832f076431272c3907dd9d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

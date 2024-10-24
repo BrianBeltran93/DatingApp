546e52c96a1f0d2f1088059cc0d9f3f05ddc4381
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-10-23 20:23:09 -0400

    MemberDto and PhotoDto created

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cab74647453066cff5d8fc07e6fd29d4717cba0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d77310b769ce77a72988638f9fe92df8fb09be8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

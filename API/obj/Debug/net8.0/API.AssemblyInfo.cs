0394bb38e3ba1ec5b7bc880bef702b67074f645a
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-10-23 22:18:31 -0400

    Member cards created

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec4c678126261564957ec5b06769722bf3a5e80f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dc18d1a691b37e5537d8c114c570adcdab54b56")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

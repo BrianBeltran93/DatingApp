4b9f042e6a3b0286323fd5df623f53a42c83b9ef
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-10-23 21:16:23 -0400

    Member and photo interface created

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1004ea67360cc120dab557da024b26a856900ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+815f41f14b34b26e716a6845d7592c60b0d0656a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

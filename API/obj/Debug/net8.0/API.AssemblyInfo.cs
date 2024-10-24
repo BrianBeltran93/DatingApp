f1004ea67360cc120dab557da024b26a856900ed
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-10-23 20:38:20 -0400

    AutoMapper being used in UsersController

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+546e52c96a1f0d2f1088059cc0d9f3f05ddc4381")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5a106240a9e42d2ed765b5619941f7bb80d9528")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

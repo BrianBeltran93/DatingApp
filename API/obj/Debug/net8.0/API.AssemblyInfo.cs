4f694e8b8c9017808fd7ae7ec79fd2902e9ea900
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-10-27 18:12:09 -0400

    Update functionality in API added

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d78ab12a1138b5bc4eac5662372c91995184ca09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0abe0181d73821822910aa7f576de2de328b2a68")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

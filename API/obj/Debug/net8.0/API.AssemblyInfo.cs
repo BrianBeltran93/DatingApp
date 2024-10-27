ce681319539e7ee566507fd480aa73e9c4dabfbb
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-10-27 16:18:03 -0400

    Edit template form created

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3a15592e90c166e3ba5a0dac9a6a533e77c9373")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17846c0db84f1e36dd33750d9ca26ff777279aaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

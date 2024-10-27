0abe0181d73821822910aa7f576de2de328b2a68
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-10-27 17:18:36 -0400

    CanDeactivate route guard added

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce681319539e7ee566507fd480aa73e9c4dabfbb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d78ab12a1138b5bc4eac5662372c91995184ca09")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

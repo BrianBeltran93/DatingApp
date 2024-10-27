0bdece263f7cb0006482642c727e543f4bc61726
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-10-27 18:43:38 -0400

    Loading indicators added

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f694e8b8c9017808fd7ae7ec79fd2902e9ea900")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c92a3348f77fd21ffc7b22f6d8e529f8ecc98c0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

3379b6d5f8a1f219e12f59578f62b802005cc460
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-10-28 21:26:59 -0400

    HttpPost "add-photo" created, ClaimsPrincipleExtension created

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19da645ebc4c869df4f1b38fbbdcd11d813db37e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a2642ad50aa1f96af6da56044d72208a95eb3ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

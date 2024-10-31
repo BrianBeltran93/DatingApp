c35e30d566ae620cfdc4421add49855db97dc2ff
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-10-30 20:03:35 -0400

    Photo uploader added + API ability to set main photo

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0471a085a87999e5d285553534715f332a7499a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4bbc1b6e04de1be64b5aefac7ba13a3b51ee74a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

dacb1665f70daa8315be26e538c90668a182a737
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-10-30 21:24:09 -0400

    reactive forms intro

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c4c73fa7cbbe6fae44696bb0ffa61dbd57c4663")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dcc44df68a4c20b3a868b6c6d0d8094e26ae5d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

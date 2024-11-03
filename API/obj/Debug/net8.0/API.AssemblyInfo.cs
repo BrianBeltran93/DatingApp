06ea76fbe3d7f9d61e164878585992a6087566ae
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-11-03 18:48:11 -0500

    Client side registration

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa7ad2c43a5e3f1cf6454f5a6ebc41e345173574")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b8df64106eb43b63ce7213b3cc9df5c7cd04ca4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

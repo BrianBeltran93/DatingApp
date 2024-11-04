0b75521341ed50dad32a8346e69b116ce0b56a82
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-11-03 20:30:17 -0500

    Angular bootstrap pagination added

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a2d13bf2dae4537f6778bd54129a72201157a2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b86e92e907b03e01475356ca704536841ddbf78e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

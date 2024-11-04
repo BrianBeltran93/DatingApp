0bc5147f5b346609ca0bd8f1b742c30f5b05c377
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-11-03 23:42:11 -0500

    Filters added API-side

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b86e92e907b03e01475356ca704536841ddbf78e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b75521341ed50dad32a8346e69b116ce0b56a82")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

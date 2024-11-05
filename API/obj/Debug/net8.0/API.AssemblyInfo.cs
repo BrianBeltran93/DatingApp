8bf9b69e4e74453d3da7853fb9d8a6fd880ac3b7
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-11-04 21:56:10 -0500

    Added an action filter

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9494fe21cfadc6e140f2edcd481165fdb943ca5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3acf4906a515f6f10b567f6b4bc7985f689c1a15")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

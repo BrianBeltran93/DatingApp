02bf9486dbcb6fd66d2f4837937d6677a003db4f
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-11-04 19:57:13 -0500

    Added filter buttons to client

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bc5147f5b346609ca0bd8f1b742c30f5b05c377")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a973c3f730deec45a226930dcec989028ac93cc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

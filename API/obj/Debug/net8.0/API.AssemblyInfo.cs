2bb0d91de1fc492769d0786a4dbf1215bb987c5c
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-11-07 21:25:38 -0500

    Pagination added for likes - API

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a8867cf9392d98313a3a79467a5013266ca34a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8ad15c50d635c52ab050fd368e64e152181eaae")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

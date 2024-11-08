dd57f4a2519f5f210e6802794f998432f2be7b5e
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-11-07 20:58:07 -0500

    toggle like functionality added

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faf028a639b689377acb9cbf3cdbc2e1c69b6a8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a8867cf9392d98313a3a79467a5013266ca34a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

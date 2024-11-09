38d7e4db351b3765019cf5c189172e42c2ba9c6e
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-11-08 20:49:15 -0500

    Automapper profiles set up

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebb202c7149043f114c2bb232132cf174a6ce909")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d6cc1b2bfcb5e40de2ec94aa796dd0aa5b24553")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

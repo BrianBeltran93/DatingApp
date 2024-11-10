2126c51ed410687a6c5947c838e542dd154bcf94
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-11-09 22:51:33 -0500

    Deleting messages client-side

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cdf4e4ebfcfc72b70a5eca76202c53bba769473")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60f0f16357fc9d89c3cf19a65061e0b95b6b8090")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

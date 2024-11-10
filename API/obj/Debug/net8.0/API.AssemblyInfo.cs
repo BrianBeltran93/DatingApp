aa470582fe08a026a394ee57e9c1f281a1eed1b5
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-11-09 20:23:27 -0500

    Message thread added in client

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48d8a680e97e41933582e860513c2f0efc54c726")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9587d2eb01365806852bf242d725f3107e22e85b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

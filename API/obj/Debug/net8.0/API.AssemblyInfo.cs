c568da8af6573dc29c2ec37f9a80c07d29b5b83b
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-11-10 20:11:47 -0500

    Get users with roles

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+079379094324b793c4b8ae2f0ffcfac65a26dadc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbe600aa9a9ff08594108ed037fb27a07b8c6785")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

f08cdb496970539095f7c21412585317a9a9dbc4
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-11-10 21:34:50 -0500

    Custom directive for admin nav added

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36acfeedbb8311e1c9d50094887a8cf40aaecf5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bba06bb4d604f91a28d8529544e1b913907abe2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

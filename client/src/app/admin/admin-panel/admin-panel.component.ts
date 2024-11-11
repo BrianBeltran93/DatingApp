60289d2ded7034dd7d1bd010140ae6a901f3162e
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-11-10 21:51:44 -0500

    Edit roles component added

--- client/src/app/admin/admin-panel/admin-panel.component.ts
+++ client/src/app/admin/admin-panel/admin-panel.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { UserManagementComponent } from "../user-management/user-management.component";
+import { HasRoleDirective } from '../../_directives/has-role.directive';
+import { PhotoEditorComponent } from "../../members/photo-editor/photo-editor.component";
+import { PhotoManagementComponent } from "../photo-management/photo-management.component";
 
 @Component({
   selector: 'app-admin-panel',
   standalone: true,
-  imports: [],
+  imports: [TabsModule, UserManagementComponent, HasRoleDirective, PhotoManagementComponent],
   templateUrl: './admin-panel.component.html',
   styleUrl: './admin-panel.component.css'
 })

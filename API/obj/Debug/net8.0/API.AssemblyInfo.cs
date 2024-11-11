8e2462b96f3219f3750aeab26fb38643ded2a149
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-11-10 19:43:04 -0500

    Roles added to app

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c92fe0e23ccc26a50f349c78fec64d0478dd7e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86b54084ffc8a2892acb1d2e82394a9372360153")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

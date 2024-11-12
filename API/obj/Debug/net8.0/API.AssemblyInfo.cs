bfe19887896fa31cc6072ea291cdfa858e81e3e2
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-11-11 22:37:08 -0500

    Authenticating to SignalR

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+720c34fd986293bc1a9f939ae5dbbc6967cb4e4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53a7954182ccbe4e63bcb1b5090182ff2f18a9ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

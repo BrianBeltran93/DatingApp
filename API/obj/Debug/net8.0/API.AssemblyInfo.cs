74a16bd3f7971929397ab102edb9d3f3fcfe1710
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-11-11 23:20:24 -0500

    Online presence displayed

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+574a16493cb298b6339fa50faa93a3c4e5714abf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2f3b4c20eab91e9d56e9c603e385de4708d9ca9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

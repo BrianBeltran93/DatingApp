2a3dab1df27a019d2a1c3c3a0e4e396379b4402d
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-11-12 21:52:59 -0500

    Message components refactoring

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+badea440e69b84d48e9f08f9eca4943160727006")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1376a35cbf89e16445bd5a07ce8b9f6c0f327ba6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

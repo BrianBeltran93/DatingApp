3db9b99e2d98d09e2b8d1ec07c723c56e9ce7f12
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-11-12 22:44:09 -0500

    tracking message groups

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a3dab1df27a019d2a1c3c3a0e4e396379b4402d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6030ec6a4c6f4014bb5548da03d6d133668d8111")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

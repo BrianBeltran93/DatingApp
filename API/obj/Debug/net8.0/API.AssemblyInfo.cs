0b4a19e489fdd25fa7df8cd7aa5d2ff62187b7ff
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-11-16 22:13:32 -0500

    Message read bug fix

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb30edb43ca772f4713ca067c4f2424d21de7f5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc6ebe060559065a611b6a8f17b335afd262e9c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

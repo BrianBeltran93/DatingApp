5fbdab07e9e105e2d722b38b11c8ac107b497746
Brian Beltran <103960101+BrianBeltran93@users.noreply.github.com>
2024-11-26 01:00:08 -0500

    Using sql server

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1a86a05c302811918dfebeacbbf8c614a2ed42e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cd16580b5a78a979dd2c3d17a02d5bdb2054f6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
